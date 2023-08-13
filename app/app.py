969281c60369d553fbd712e927c8480ef651b68e
clairolzamcs <clairolzamsalazar@gmail.com>
2023-08-13 15:09:36 +0000

    Supplying default DBPORT

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ DBUSER = os.environ.get("DBUSER") or "root"
 DBPWD = os.environ.get("DBPWD") or "passwors"
 DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
-DBPORT = int(os.environ.get("DBPORT"))
+DBPORT = int(os.environ.get("DBPORT")) or 3306
 
 # Background image downloaded through init container
 BGIMG = "tmp/background.jpg"
